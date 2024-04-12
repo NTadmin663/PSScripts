629c4fc96d5c6caa825bea132ce5d1fb7909cb17
NTadmin663 <166816808+NTadmin663@users.noreply.github.com>
2024-04-12 16:29:06 +0530

    Add files via upload
    Instructions to combine Autopilot Hardware Hash CSVs
    
    1.Keep the AutopilotCSVsToBeMerged.ps1 file inside the folder where all Hardware Hash CSV files are located.
    2. Just right click and execute the PS script .
    3. Once execution is completed, at the same location It will create a folder called AutopilotCSVMerged and inside that folder you can get CSV file with all CSVs details combined.

---
+++ AutopilotCSVsToBeMerged.ps1
@@ -0,0 +1,30 @@
+﻿######### Script to Merge AutoPilot Hardware Hash Multiple CSV Files #####
+
+# Created NTAdmin #
+
+# Owned By NT Admin #
+
+#Parameters
+$sourceFolder = "$PSscriptroot\"
+
+
+Get-ChildItem -Path $PSscriptroot\AutopilotCSVMerged -Recurse | Remove-Item -force -recurse -EA SilentlyContinue
+Remove-Item '$PSscriptroot\AutopilotCSVMerged' -force -EA SilentlyContinue
+
+
+New-Item -Path "$PSscriptroot\AutopilotCSVMerged" -ItemType "Directory" -Force
+
+$outputFile = "$PSscriptroot\AutopilotCSVMerged\AutopilotCSVsMerged.csv"
+ 
+$SourceFiles = Get-ChildItem -Path $sourceFolder -Filter *.csv
+ 
+$FirstFile = $true
+ForEach ($File in $SourceFiles) {
+    $Content = Get-Content $File.FullName
+ 
+    if ($firstFile) {
+        $content[0] | Add-Content $outputFile
+        $firstFile = $false
+    }
+    $content | Select-Object -Skip 1 | Add-Content $outputFile
+}
